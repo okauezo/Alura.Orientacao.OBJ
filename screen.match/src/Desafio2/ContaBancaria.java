b2af4f6b22792902af79fd444aa581208990a575
okauezo <kauepiresmoreira09@gmail.com>
2025-11-04 21:46:01 -0300

    criando a classe IdadePessoa e metodos gets e sets

--- screen.match/src/Desafio2/ContaBancaria.java
+++ screen.match/src/Desafio2/ContaBancaria.java
@@ -1,4 +1,35 @@
 package Desafio2;
 
 public class ContaBancaria {
+
+    //Crie uma classe ContaBancaria com os seguintes atributos: numeroConta (privado), saldo (privado) e titular (publico).
+    // Implemente métodos getters e setters para os atributos privados.
+
+    private int numeroConta;
+    private double saldo;
+    String titular;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
 }
---
+++ screen.match/src/Desafio2/IdadePessoa.java
@@ -0,0 +1,29 @@
+package Desafio2;
+
+public class IdadePessoa {
+
+    // Crie uma classe IdadePessoa com os atributos privados nome e idade.
+    // Utilize métodos getters e setters para acessar e modificar esses atributos.
+    // Adicione um método verificarIdade que imprime se a pessoa é maior de idade ou não.
+
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+}
