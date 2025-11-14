9b5e84f6bc8a6172687651e8e183dc9fecd50d47
okauezo <kauepiresmoreira09@gmail.com>
2025-11-13 21:49:36 -0300

    Criando classe Serie

--- screen.match/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screen.match/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,5 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private
+public class Filme extends Titulo{
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 }
