9e311cacbee73e3776cde0ce47705bf87b41d86b
okauezo <kauepiresmoreira09@gmail.com>
2025-11-16 21:56:37 -0300

    aplicação de Conta poupanca e herança

--- screen.match/src/br/com/alura/screenmatch/modelos/contaBancaria/ContaPoupanca.java
+++ screen.match/src/br/com/alura/screenmatch/modelos/contaBancaria/ContaPoupanca.java
@@ -1,4 +1,12 @@
 package br.com.alura.screenmatch.modelos.contaBancaria;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends ContaBanco{
+
+    private double taxaDeJuros;
+
+    public void calcularJuros(){
+        double juros = this.getSaldo() * taxaDeJuros;
+        System.out.println("juros atual: " + juros);
+    }
+
 }
