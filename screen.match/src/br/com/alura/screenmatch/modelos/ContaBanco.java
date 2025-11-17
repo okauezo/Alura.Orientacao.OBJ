de282ed2cb429f90dfa3eeecea9d02041d765343
okauezo <kauepiresmoreira09@gmail.com>
2025-11-16 21:30:48 -0300

    metodos da class mãe criada

--- screen.match/src/br/com/alura/screenmatch/modelos/ContaBanco.java
+++ screen.match/src/br/com/alura/screenmatch/modelos/ContaBanco.java
@@ -2,4 +2,25 @@ package br.com.alura.screenmatch.modelos;
 
 public class ContaBanco {
 
+    private String titula;
+    private double saldo;
+
+    public void sacar(double valor){
+        if (valor <= 0){
+            System.out.println("Valor tem que ser maior que zero!");
+        } else if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso! Saldo atual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+    public void depositar(double valor){
+        if (valor >= 0){
+            saldo += valor;
+            System.out.println("Depositado com sucesso! Saldo atual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
 }
