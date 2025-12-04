14df2608b955b25eb148d8b8d5842818952c63e3
okauezo <kauepiresmoreira09@gmail.com>
2025-12-04 16:44:05 -0300

    criando o desafio 3

---
+++ screen.match/src/Desafio3/Carro.java
@@ -0,0 +1,10 @@
+package Desafio3;
+
+public class Carro {
+
+//    Crie uma classe Carro com métodos para representar um modelo específico ao longo de três anos.
+//    Implemente métodos para definir o nome do modelo, os preços médios para cada ano, e calcular e exibir o menor e o maior preço.
+//    Adicione uma subclasse ModeloCarro para criar instâncias específicas, utilizando-a na classe principal
+//    para definir preços e mostrar informações.
+
+}
