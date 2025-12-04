b375b200a0a7a5eb528de729028c813d817d0ebe
okauezo <kauepiresmoreira09@gmail.com>
2025-12-04 18:20:03 -0300

    classe cachorro criada, herdad

--- screen.match/src/Desafio3/Animal.java
+++ screen.match/src/Desafio3/Animal.java
@@ -6,4 +6,7 @@ public class Animal {
 //    Adicione o método emitirSom() nas subclasses, utilizando a anotação @Override para indicar que estão sobrescrevendo o método.
 //    Além disso, adicione métodos específicos para cada subclasse, como abanarRabo() para o Cachorro e arranharMoveis() para o Gato.
 
+    public void emitirSom(){
+        System.out.println("Emitido Som");
+    }
 }
---
+++ screen.match/src/Desafio3/Cachorro.java
@@ -0,0 +1,11 @@
+package Desafio3;
+
+public class Cachorro extends Animal {
+
+    public void emitirSom(){
+        System.out.println("au au au");
+    }
+    public void abanarRabo(){
+        System.out.println("Abanando Rabo");
+    }
+}
