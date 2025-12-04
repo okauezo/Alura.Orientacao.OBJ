de2e9703b2330a45784de7e33ec6d79715b0e77f
okauezo <kauepiresmoreira09@gmail.com>
2025-12-04 18:25:34 -0300

    subcless gato criada e herdando

--- screen.match/src/Desafio3/Cachorro.java
+++ screen.match/src/Desafio3/Cachorro.java
@@ -3,9 +3,9 @@ package Desafio3;
 public class Cachorro extends Animal {
 
     public void emitirSom(){
-        System.out.println("au au au");
+        System.out.println("AU AU AU");
     }
     public void abanarRabo(){
-        System.out.println("Abanando Rabo");
+        System.out.println("Abanando o Rabo");
     }
 }
---
+++ screen.match/src/Desafio3/Gato.java
@@ -0,0 +1,11 @@
+package Desafio3;
+
+public class Gato extends Animal{
+    public void emitirSom(){
+        System.out.println("Miau Miau");
+    }
+
+    public void arranharMoveis() {
+        System.out.println("Arranhando o Moveis");
+    }
+}
