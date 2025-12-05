c85e6310f6b4af095b87e410bf0bc96a5a6e2817
okauezo <kauepiresmoreira09@gmail.com>
2025-12-05 19:02:19 -0300

    metodos criados em Conta bancaria

--- screen.match/src/Desafio3/ContaBancaria.java
+++ screen.match/src/Desafio3/ContaBancaria.java
@@ -1,7 +1,32 @@
 package Desafio3;
 
 public class ContaBancaria {
+
 //    Crie uma classe ContaBancaria com métodos para realizar operações bancárias como depositar(), sacar() e consultarSaldo().
 //    Em seguida, crie uma subclasse ContaCorrente que herda da classe ContaBancaria.
 //    Adicione um método específico para a subclasse, como cobrarTarifaMensal(), que desconta uma tarifa mensal da conta corrente.
+
+    private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double depositar(double valor) {
+        saldo += valor;
+        return saldo;
+    }
+
+    public double sacar(double valor) {
+        saldo -= valor;
+        return saldo;
+    }
+
+    public void  consultarSaldo(){
+        System.out.println("Saldo atual: " + saldo);
+    }
 }
