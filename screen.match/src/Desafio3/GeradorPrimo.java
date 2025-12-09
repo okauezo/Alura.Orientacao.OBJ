fab63dd012f84db3c8542b927a1536b87cd7b4c5
okauezo <kauepiresmoreira09@gmail.com>
2025-12-09 14:30:46 -0300

    SubClasse Gerador Primo

---
+++ screen.match/src/Desafio3/GeradorPrimo.java
@@ -0,0 +1,12 @@
+package Desafio3;
+
+public class GeradorPrimo extends NumerosPrimos{
+        public int gerarProximoPrimo(int ultimoPrimoConhecido) {
+            int proximoNumero = ultimoPrimoConhecido + 1;
+            while (!verificarPrimalidade(proximoNumero)) {
+                proximoNumero++;
+            }
+            return proximoNumero;
+        }
+    }
+
