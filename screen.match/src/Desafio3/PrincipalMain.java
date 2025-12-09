e0ce64fb7c9d3edc5e17ba811f5e640fb9a2de4b
okauezo <kauepiresmoreira09@gmail.com>
2025-12-09 14:39:13 -0300

    Main funcionando

--- screen.match/src/Desafio3/PrincipalMain.java
+++ screen.match/src/Desafio3/PrincipalMain.java
@@ -27,5 +27,17 @@ public class PrincipalMain {
         conta.depositar(900);
         conta.sacar(690);
         System.out.println("Saldo Atualizado: R$ " + conta.getSaldo());
+
+        System.out.println();
+
+        VerificadorPrimo verificador = new VerificadorPrimo();
+        verificador.verificarSeEhPrimo(17);
+
+        GeradorPrimo gerador = new GeradorPrimo();
+        int proximoPrimo = gerador.gerarProximoPrimo(17);
+        System.out.println("O próximo primo após 17 é: " + proximoPrimo);
+
+        NumerosPrimos numerosPrimos = new NumerosPrimos();
+        numerosPrimos.listarPrimos(30);
     }
 }
