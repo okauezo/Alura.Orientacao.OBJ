722372128a15e805ee9a1b04134cb8c88eb06da9
okauezo <kauepiresmoreira09@gmail.com>
2025-12-19 22:18:52 -0300

    Classe Servico Criado

--- screen.match/src/imposto/Produto.java
+++ screen.match/src/imposto/Produto.java
@@ -10,4 +10,20 @@ public class Produto implements Tributavel {
     public double getValorImposto() {
         return this.valor * 0.1;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
