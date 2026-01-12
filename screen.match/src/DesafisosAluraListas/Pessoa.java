7a6f09d7cda926f0408b098aa3a85f2013da9611
okauezo <kauepiresmoreira09@gmail.com>
2026-01-12 16:27:07 -0300

    Metodos Pessso feito e usando o toString

--- screen.match/src/DesafisosAluraListas/Pessoa.java
+++ screen.match/src/DesafisosAluraListas/Pessoa.java
@@ -1,4 +1,18 @@
 package DesafisosAluraListas;
 
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private double altura;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Pessoa{");
+        sb.append("nome='").append(nome).append('\'');
+        sb.append(", Idade=").append(idade);
+        sb.append(", altura=").append(altura);
+        sb.append('}');
+        return sb.toString();
+    }
 }
