6956b69158ac1d59b3914af223ca0118498b80f8
okauezo <kauepiresmoreira09@gmail.com>
2026-01-15 22:25:12 -0300

    Classe Produto com seus atributos e toString feito

--- screen.match/src/DesafisosAluraListas/Produto.java
+++ screen.match/src/DesafisosAluraListas/Produto.java
@@ -1,4 +1,26 @@
 package DesafisosAluraListas;
 
+import java.util.ArrayList;
+
 public class Produto {
+
+        private String  nome;
+        private double preco;
+        private int quantidade;
+
+        public Produto(String nome, double preco, int quantidade) {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Produto{");
+        sb.append("nome= '").append(nome).append('\'');
+        sb.append(", preco= ").append(preco);
+        sb.append(", quantidade= ").append(quantidade);
+        sb.append('}');
+        return sb.toString();
+    }
 }
