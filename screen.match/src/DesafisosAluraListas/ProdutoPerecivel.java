d923750f7c7d099aad027360fbc2a237b61b2c34
okauezo <kauepiresmoreira09@gmail.com>
2026-01-15 22:51:28 -0300

    Implementei private LocalDate dataValidade para dar data de validade

--- screen.match/src/DesafisosAluraListas/ProdutoPerecivel.java
+++ screen.match/src/DesafisosAluraListas/ProdutoPerecivel.java
@@ -3,9 +3,11 @@ package DesafisosAluraListas;
 import java.time.LocalDate;
 public class ProdutoPerecivel extends Produto {
 
+    private LocalDate dataValidade;
 
-
-    public ProdutoPerecivel(String nome, double preco, int quantidade) {
+    public ProdutoPerecivel(String nome, double preco, int quantidade, LocalDate dataValidade) {
         super(nome, preco, quantidade);
+        this.dataValidade = dataValidade;
     }
+
 }
