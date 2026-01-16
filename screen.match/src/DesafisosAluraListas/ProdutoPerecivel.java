de283a0557f14d67635cbc98fee024cbba9d3f31
okauezo <kauepiresmoreira09@gmail.com>
2026-01-15 22:49:14 -0300

    Criei a Classe ProdutoPerecivel e herdei de Produto, coloquei super para herdar

---
+++ screen.match/src/DesafisosAluraListas/ProdutoPerecivel.java
@@ -0,0 +1,11 @@
+package DesafisosAluraListas;
+
+import java.time.LocalDate;
+public class ProdutoPerecivel extends Produto {
+
+
+
+    public ProdutoPerecivel(String nome, double preco, int quantidade) {
+        super(nome, preco, quantidade);
+    }
+}
