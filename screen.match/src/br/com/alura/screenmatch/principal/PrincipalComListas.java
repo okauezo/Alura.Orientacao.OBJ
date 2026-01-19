df9c59b1d690ffde0eaa272c7dbb4f89a7453977
okauezo <kauepiresmoreira09@gmail.com>
2026-01-18 21:21:17 -0300

    Serie ainda com erro exption mais imprimindo, usando um novo for (forEach)

--- screen.match/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screen.match/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -1,7 +1,33 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
 public class PrincipalComListas {
     static void main() {
 
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(7);
+        Filme outroFilme = new Filme("Vingadores", 2014);
+        outroFilme.avalia(6);
+        var filmeDoKaue = new Filme("Interistelar", 2016);
+        filmeDoKaue.avalia(10);
+        Serie strangthings = new Serie("Strang Things", 2016);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(filmeDoKaue);
+        lista.add(strangthings);
+
+        for(Titulo item: lista) {
+            System.out.println(item);
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classificação: " + filme.getClassificavel());
+        }
     }
 }
