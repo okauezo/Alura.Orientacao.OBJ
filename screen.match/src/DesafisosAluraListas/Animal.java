b1b4696a11f6b26083774e53086cdbc5557b8abc
okauezo <kauepiresmoreira09@gmail.com>
2026-01-18 22:20:19 -0300

    Classe Cachorro herdando do (pai) Animal e usando o super

--- screen.match/src/DesafisosAluraListas/Animal.java
+++ screen.match/src/DesafisosAluraListas/Animal.java
@@ -1,4 +1,14 @@
 package DesafisosAluraListas;
 
 public class Animal {
+
+    private String correr;
+    private String beberAgua;
+    private String comer;
+
+    public Animal(String correr, String beberAgua, String comer) {
+        this.correr = correr;
+        this.beberAgua = beberAgua;
+        this.comer = comer;
+    }
 }
--- screen.match/src/DesafisosAluraListas/Cachorro.java
+++ screen.match/src/DesafisosAluraListas/Cachorro.java
@@ -1,4 +1,13 @@
 package DesafisosAluraListas;
 
-public class Cachorro {
+public class Cachorro extends Animal {
+
+
+    public void latir(){
+        System.out.println("au au");
+    }
+
+    public Cachorro(String correr, String beberAgua, String comer) {
+        super(correr, beberAgua, comer);
+    }
 }
