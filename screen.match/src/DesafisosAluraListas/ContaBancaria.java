1a555347e7a71bee513ffb7325f630259c62062d
okauezo <kauepiresmoreira09@gmail.com>
2026-01-22 23:19:18 -0300

    Main com metodos de listas e  loop para pegar a conta com o maior saldo

--- screen.match/src/DesafisosAluraListas/ContaBancaria.java
+++ screen.match/src/DesafisosAluraListas/ContaBancaria.java
@@ -2,6 +2,29 @@ package DesafisosAluraListas;
 
 public class ContaBancaria {
 
+    private String cliente;
     private double numeroDaConta;
     private double saldo;
+
+    public ContaBancaria(String cliente, double numeroDaConta, double saldo) {
+        this.cliente = cliente;
+        this.numeroDaConta = numeroDaConta;
+        this.saldo = saldo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public double getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
--- screen.match/src/DesafisosAluraListas/ContaCliente.java
+++ screen.match/src/DesafisosAluraListas/ContaCliente.java
@@ -1,4 +1,25 @@
 package DesafisosAluraListas;
 
+import java.util.ArrayList;
+
 public class ContaCliente {
+    static void main() {
+
+    ArrayList<ContaBancaria> contas = new ArrayList<>();
+    contas.add(new ContaBancaria("Sarah", 4879936,10000));
+    contas.add(new ContaBancaria("Tania", 7567854, 2000000));
+    contas.add(new ContaBancaria("Zac", 4123936,100));
+
+    ContaBancaria maiorConta = contas.get(0);
+    for(int i = 1; i < contas.size(); i++) {
+        if(contas.get(i).getSaldo() > maiorConta.getSaldo()) {
+            maiorConta = contas.get(i);
+        }
+    }
+        System.out.println("Conta com maior saldo:");
+        System.out.println("Cliente: " + maiorConta.getCliente());
+        System.out.println("Número da conta: " + maiorConta.getNumeroDaConta());
+        System.out.println("Saldo: R$ " + maiorConta.getSaldo());
+
+    }
 }
