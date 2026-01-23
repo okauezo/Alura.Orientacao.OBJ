7c548bddceedd1edc6d217ab1bec6b839ff9cc35
okauezo <kauepiresmoreira09@gmail.com>
2026-01-22 22:38:24 -0300

    Calculando raio de circulo e lado de quadrado

--- screen.match/src/DesafisosAluraListas/PrincipalFormulas.java
+++ screen.match/src/DesafisosAluraListas/PrincipalFormulas.java
@@ -1,7 +1,17 @@
 package DesafisosAluraListas;
 
+import java.util.ArrayList;
+
 public class PrincipalFormulas {
     static void main() {
+        ArrayList<Formula> formas = new ArrayList<>();
+        formas.add(new Circulo(1.2));
+        formas.add(new Quadrado(4));
+        formas.add(new Circulo(1.5));
+        formas.add(new Quadrado(8));
 
+        for(Formula f: formas) {
+            System.out.println(f.calcularArea());
+        }
     }
 }
