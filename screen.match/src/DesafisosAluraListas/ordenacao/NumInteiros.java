78557de3b5cf60987bbef0c89250ee64bc51c3f1
okauezo <kauepiresmoreira09@gmail.com>
2026-02-04 21:30:55 -0300

    Classe NumInteiros, Criei um Objeto com parameto Int e fiz uma lista dele, usei List ArrayList para fazer essa lista, utilizei tambem o Collections.sort para ordenar a lista junto do CompareTo.

--- screen.match/src/DesafisosAluraListas/ordenacao/NumInteiros.java
+++ screen.match/src/DesafisosAluraListas/ordenacao/NumInteiros.java
@@ -1,4 +1,26 @@
 package DesafisosAluraListas.ordenacao;
 
-public class NumInteiros {
+import org.jetbrains.annotations.NotNull;
+
+public class NumInteiros implements Comparable<NumInteiros> {
+
+    private int numeros;
+
+    public NumInteiros(int numeros) {
+        this.numeros = numeros;
+    }
+
+    public int getNumeros() {
+        return numeros;
+    }
+
+    @Override
+    public int compareTo(NumInteiros outroNumInteiros) {
+        return this.getNumeros() - outroNumInteiros.getNumeros();
+    }
+
+    @Override
+    public String toString() {
+        return "Numeros =(" +  numeros + ")";
+    }
 }
--- screen.match/src/DesafisosAluraListas/ordenacao/Principal.java
+++ screen.match/src/DesafisosAluraListas/ordenacao/Principal.java
@@ -1,7 +1,26 @@
 package DesafisosAluraListas.ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Principal {
     static void main() {
 
+        NumInteiros num1 = new NumInteiros(1);
+        NumInteiros num3 = new NumInteiros(15);
+        NumInteiros num2 = new NumInteiros(2);
+        NumInteiros num4 = new NumInteiros(10);
+        NumInteiros num5 = new NumInteiros(70);
+
+        List<NumInteiros> lista = new ArrayList<>();
+        lista.add(num1);
+        lista.add(num2);
+        lista.add(num3);
+        lista.add(num4);
+        lista.add(num5);
+
+        Collections.sort(lista);
+        System.out.println(lista);
     }
 }
